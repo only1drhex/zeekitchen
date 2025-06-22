6e96a0d9286d36f2903230b83e641fc156b592ac
Drhex <96548339+only1drhex@users.noreply.github.com>
2025-06-22 07:26:29 +0100

    Update page.tsx

--- src/app/url/page.tsx
+++ src/app/url/page.tsx
@@ -1,21 +1,21 @@
-// pages/ssr-test.js
-export async function getServerSideProps() {
-  const timestamp = new Date().toISOString();
 
-  return {
+
+export default function SSRTest({ message, timestamp }) {
+
+   const timestamp = new Date().toISOString();
+
+  const obj = {
     props: {
       message: "This page was rendered on the server",
       timestamp,
     },
   };
 }
-
-export default function SSRTest({ message, timestamp }) {
   return (
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>SSR Test</h1>
-      <p>{message}</p>
-      <p>Server Time: {timestamp}</p>
+      <p>{obj.props.message}</p>
+      <p>Server Time: {obj.props.timestamp}</p>
     </div>
   );
 }
